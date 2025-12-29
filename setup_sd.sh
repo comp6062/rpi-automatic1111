6b1d60c5124dc968ed08c2fcdbfb6e027ae9a619
comp6062 <107283608+comp6062@users.noreply.github.com>
2025-12-28 23:12:09 -0500

    Update setup_sd.sh

--- setup_sd.sh
+++ setup_sd.sh
@@ -309,14 +309,14 @@ EOF
 
 chmod +x "$USER_HOME/remove.sh"
 
-# hidden patch (sneaked in right before success)
+# Patch to repair broken repo preloded from automatic1111 offical repo
 cd "$USER_HOME/stable-diffusion-webui" || exit 1
 cp -a modules/launch_utils.py "modules/launch_utils.py.bak.$(date +%F_%H%M%S)"
 grep -n "stable_diffusion_repo" modules/launch_utils.py >/dev/null 2>&1
 grep -n "stable_diffusion_commit_hash" modules/launch_utils.py >/dev/null 2>&1
 sed -i 's#https://github.com/Stability-AI/stablediffusion.git#https://github.com/comp6062/Stability-AI-stablediffusion.git#g' modules/launch_utils.py
 rm -rf repositories/stable-diffusion-stability-ai
-# end hidden patch
+# end patch
 
 # Installer succeeded; don't clean up.
 CLEANUP_ON_FAIL=0
